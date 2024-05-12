df6b9567e9781ad5b1255025c677eb9695975849
royossai <royossai@yahoo.com>
2024-05-12 17:05:09 +0200

    testing with star bird

--- scripts/create_cilium.sh
+++ scripts/create_cilium.sh
@@ -14,6 +14,9 @@ create_cilium() {
       --set=kubeProxyReplacement=true \
       --set=securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
       --set=securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+      --set ingressController.enabled=true \
+      --set ingressController.loadbalancerMode=dedicated \
+      --set ingressController.default=true \
       --set=cgroup.autoMount.enabled=false \
       --set=cgroup.hostRoot=/sys/fs/cgroup \
       --set=k8sServiceHost=localhost \
