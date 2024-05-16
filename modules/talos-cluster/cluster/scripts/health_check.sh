7bc328300b0824a06044d6608cbc16b17d427677
royossai <royossai@yahoo.com>
2024-05-16 12:28:26 +0200

    testing with star bird

--- modules/talos-cluster/cluster/scripts/health_check.sh
+++ modules/talos-cluster/cluster/scripts/health_check.sh
@@ -22,30 +22,6 @@ check_health() {
   echo "[INFO] Successfully performed health check on the cluster"
 }
 
-# This function assumes that the first argument is a github username, the second argument is a github repository name,
-# the third argument is the github token and the fourth argument is the cluster name
-# It initializes the fluxcd
-initialize_fluxcd() {
-  local github_username=$1
-  local github_repo=$2
-  local github_token=$3
-  local cluster_name=$4
-
-  echo "[INFO] Initializing the fluxcd"
-
-  export GITHUB_TOKEN="${github_token}"
-
-  flux bootstrap github \
-    --owner="${github_username}" \
-    --repository="${github_repo}" \
-    --branch=main \
-    --path=clusters/"${cluster_name}" \
-    --personal \
-    --token-auth \
-
-  echo "[INFO] Successfully initialized the fluxcd"
-}
-
 # This function assumes that the first argument is an IP address, the second argument is the talos directory
 # The third argument is list of master node ips in format "master-0,master-1,master-2" and
 # The fourth argument is the list of worker node ips in format "worker-0,worker-1,worker-2"
@@ -90,96 +66,121 @@ label_worker_nodes() {
   echo "[INFO] Successfully labeled the worker nodes"
 }
 
+# This function assumes that the first argument is a github username, the second argument is a github repository name,
+# the third argument is the github token and the fourth argument is the cluster name
+# It initializes the fluxcd
+initialize_fluxcd() {
+  local github_username=$1
+  local github_repository=$2
+  local github_token=$3
+  local cluster_name=$4
+
+  echo "[INFO] Initializing the fluxcd"
+
+  export GITHUB_TOKEN="${github_token}"
+
+  flux bootstrap github \
+    --owner="${github_username}" \
+    --repository="${github_repository}" \
+    --branch=main \
+    --path=clusters/"${cluster_name}" \
+    --personal \
+    --token-auth \
+
+  echo "[INFO] Successfully initialized the fluxcd"
+}
+
 # This function assumes that the first argument is an IP address, the second argument is the talos directory
-# the third argument is a github username, the fourth argument is a github repository name,
-# the fifth argument is the github token, the sixth argument is the cluster name
-# the seventh argument is a list of all the worker nodes names in format "worker-0,worker-1,worker-2"
-# the eighth argument is the list of worker node ips in format "worker-0 worker-1 worker-2" and
-# the ninth argument is the list of master node ips in format "master-0 master-1 master-2"
-# It starts the health check and kubeconfig update process
+# the third argument is a list of all the worker nodes names
+# the fourth argument is the list of worker node ips in format
+# the fifth argument is the list of master node ips in format
+# the sixth argument is the github username
+# the seventh argument is the github token
+# the eighth argument is the github repository
+# the ninth argument is the cluster name
 main() {
   local primary_controller=$1
   local talos_dir=$2
-  local github_username=$3
-  local github_repo=$4
-  local github_token=$5
-  local cluster_name=$6
-  local worker_nodes=$7
-  local worker_nodes_ips=$8
-  local master_nodes_ips=$9
+  local worker_nodes_names=$3
+  local worker_nodes_ips=$4
+  local master_nodes_ips=$5
+  local github_username=$6
+  local github_token=$7
+  local github_repository=$8
+  local cluster_name=$9
 
-  update_kubeconfig "${primary_controller}" "${talos_dir}" "${master_nodes_ips}" "${worker_nodes_ips}"
   check_health "${primary_controller}" "${talos_dir}"
-  label_worker_nodes "${worker_nodes}"
-  initialize_fluxcd "${github_username}" "${github_repo}" "${github_token}" "${cluster_name}"
+  update_kubeconfig "${primary_controller}" "${talos_dir}" "${master_nodes_ips}" "${worker_nodes_ips}"
+  label_worker_nodes "${worker_nodes_names}"
+  initialize_fluxcd "${github_username}" "${github_repository}" "${github_token}" "${cluster_name}"
 }
 
-# This function builds the arguments for the main function
 build_args() {
+  local worker_nodes_names=""
+  local master_nodes_ips=""
+  local worker_nodes_ips=""
   local primary_controller=""
   local talos_dir=""
   local cluster_name=""
-  local github_username=""
-  local github_repo=""
-  local github_token=""
-  local worker_nodes=""
-  local master_nodes_ips=""
-  local worker_nodes_ips=""
+  local username=""
+  local token=""
+  local repository=""
 
-   while test $# -gt 0; do
-      case "$1" in
-          -p|--primary-controller)
-              shift
-              primary_controller=$1
-              shift
-              ;;
-          -t|--talos-dir)
-              shift
-              talos_dir=$1
-              shift
-              ;;
-          -c|--cluster-name)
-              shift
-              cluster_name=$1
-              shift
-              ;;
-          -u|--github-username)
-              shift
-              github_username=$1
-              shift
-              ;;
-          -r|--github-repo)
-              shift
-              github_repo=$1
-              shift
-              ;;
-          -k|--github-token)
-              shift
-              github_token=$1
-              shift
-              ;;
-          -w|--worker-nodes)
-              shift
-              worker_nodes=$1
-              shift
-              ;;
-          -m|--master-nodes-ips)
-              shift
-              master_nodes_ips=$1
-              shift
-              ;;
-          -i|--worker-nodes-ips)
-              shift
-              worker_nodes_ips=$1
-              shift
-              ;;
-          *)
-              break
-              ;;
-      esac
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+       -p|--primary-controller)
+         shift
+         primary_controller=$1
+         shift
+         ;;
+       -t|--talos-dir)
+         shift
+         talos_dir=$1
+         shift
+         ;;
+      -w|--worker-nodes-names)
+         shift
+         worker_nodes_names=$1
+         shift
+         ;;
+      -m|--master-nodes-ips)
+        shift
+        master_nodes_ips=$1
+        shift
+        ;;
+      -i|--worker-nodes-ips)
+        shift
+        worker_nodes_ips=$1
+        shift
+        ;;
+      -c|--cluster-name)
+        shift
+        cluster_name=$1
+        shift
+        ;;
+      -u|--username)
+        shift
+        username=$1
+        shift
+        ;;
+      -k|--token)
+        shift
+        token=$1
+        shift
+        ;;
+      -r|--repository)
+        shift
+        repository=$1
+        shift
+        ;;
+      *)
+        echo "Unknown parameter passed: $1"
+        exit 1
+        ;;
+    esac
   done
 
-  main "${primary_controller}" "${talos_dir}" "${github_username}" "${github_repo}" "${github_token}" "${cluster_name}" "${worker_nodes}" "${worker_nodes_ips}" "${master_nodes_ips}"
+  main "${primary_controller}" "${talos_dir}" "${worker_nodes_names}" "${worker_nodes_ips}" "${master_nodes_ips}" "${username}" "${token}" "${repository}" "${cluster_name}"
 }
 
 build_args "$@"
